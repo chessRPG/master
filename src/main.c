7c7af4b3e98d8fc3deb20b3181c839bec91ee42a
SCHREYECK TIFFANIE p1301249 <p1301249@b710l0908.univ-lyon1.fr>
2015-03-31 09:25:23 +0200

    main

--- src/main.c
+++ src/main.c
@@ -3,13 +3,10 @@
 int main()
 {
     Jeu jeu ;
-<<<<<<< HEAD
+
 
     jeu.plateau = *creerPlateau(BLANC, NOIR) ;
 
-=======
-    jeu.plateau = *creerPlateau(BLANC, NOIR) ;
->>>>>>> adfb435f353152098c382447728fce244c067471
     boucleEvent(&jeu) ;
     return 0 ;
 }
